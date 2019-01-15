5be551dd173e3de9572074f0bee5adab0afbf41c
Arnold Chand <arnold.chand@gmail.com>
2019-01-14 21:25:28 -0500

    Added footer component to be used for the pages

---
+++ blog/.vuepress/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer">
+    <span class="footer-content">Copyright &copy; Arnold Chand | Powered by <a href="https://vuepress.vuejs.org" target="_blank">VuePress</a></span>
+  </div>
+</template>
+
+<style>
+.footer {
+  text-align: center;
+}
+
+.footer-content {
+  font-size: 0.8em;
+  font-style: italic;
+}
+</style>
