75ef72ca29b4b8a5a17779b5c51b6b12962d7f18
Arnold Chand <creativenull@outlook.com>
2022-06-09 11:58:57 -0400

    feat: posts components for page

---
+++ src/components/page/Posts.tsx
@@ -0,0 +1,36 @@
+import { For } from "solid-js";
+import { MarkdownInstance } from "astro";
+import truncate from "lodash.truncate";
+import { config } from "@config";
+import AppLink from "@components/AppLink";
+
+export interface PostsProps {
+  posts: MarkdownInstance<Record<string, any>>[];
+}
+
+export default function Posts(props: PostsProps) {
+  const truncateOptions = {
+    length: 150,
+    separator: " ",
+  };
+
+  return (
+    <div>
+      <For each={props.posts}>
+        {(post) => (
+          <div class="space-y-4 mb-6">
+            <h1 class="post-title md:text-3xl text-xl">
+              <AppLink href={post.url}>{post.frontmatter.title}</AppLink>
+            </h1>
+            <div class="post-attr font-bold">
+              {config.author} / {post.frontmatter.date}
+            </div>
+            <p class="post-desc leading-loose md:text-xl text-lg">
+              {truncate(post.frontmatter.description, truncateOptions)}
+            </p>
+          </div>
+        )}
+      </For>
+    </div>
+  );
+}
