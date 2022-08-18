7bc86dba505a237f1293b69f0f401f2948ff8177
Arnold Chand <creativenull@outlook.com>
2022-08-17 23:25:50 -0300

    feat: use mdx with jsx components

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import WindiCSS from "vite-plugin-windicss"
 import solid from "@astrojs/solid-js";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   outDir: "./docs",
   site: 'https://creativenull.xyz',
-  integrations: [solid()],
+  integrations: [solid(), mdx()],
   vite: {
     plugins: [WindiCSS()]
   }
