6f4e8652ed6d44971e8e0277ed7160e69ca11a5b
Arnold Chand <creativenull@outlook.com>
2023-08-15 13:04:50 -0400

    feat: add md for astro ls

--- .vimrc.lua
+++ .vimrc.lua
@@ -3,7 +3,7 @@
 local user_config = require("projectlocal.lsp").get_config()
 
 require("lspconfig").astro.setup(vim.tbl_extend("force", user_config, {
-  filetypes = { "astro", "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  filetypes = { "astro", "typescript", "typescriptreact", "javascript", "javascriptreact", "markdown" },
   init_options = {
     typescript = {
       tsdk = string.format("%s/node_modules/typescript/lib", vim.call("getcwd")),
