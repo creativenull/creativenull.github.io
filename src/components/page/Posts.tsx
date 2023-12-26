a3ca30f087fd6461c520757d1c0ba14733c97e61
Arnold Chand <creativenull@outlook.com>
2023-12-26 15:57:22 -0400

    fix: rm solid components

--- src/components/page/Posts.tsx
+++ src/components/page/Posts.tsx
@@ -1,6 +1,5 @@
 import { For } from "solid-js";
 import truncate from "lodash.truncate";
-import AppLink from "@components/AppLink";
 import { CollectionEntry } from "astro:content";
 
 export interface PostsProps {
@@ -19,11 +18,14 @@ export default function Posts(props: PostsProps) {
         {(post) => (
           <div class="space-y-4 mb-6">
             <h1 class="post-title md:text-2xl text-xl">
-              <AppLink href={`/${post.collection}/${post.slug}`}>{post.data.title}</AppLink>
+              <a
+                href={`/${post.collection}/${post.slug}`}
+                class="text-blue-700 font-bold underline hover:no-underline dark:text-blue-500"
+              >
+                {post.data.title}
+              </a>
             </h1>
-            <div class="post-attr italic text-xs md:text-sm">
-              {post.data.date}
-            </div>
+            <div class="post-attr italic text-xs md:text-sm">{post.data.date}</div>
             <p class="post-desc leading-loose">
               {truncate(post.data.description, truncateOptions)}
             </p>
