a900c953310ced6cb1f2e8049f1807687363b035
Arnold Chand <creativenull@outlook.com>
2023-12-27 00:10:43 -0400

    fix: rm solidjs

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-import solid from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 import { config } from "./src/config";
 
@@ -8,5 +7,5 @@ import { config } from "./src/config";
 export default defineConfig({
   outDir: "./docs",
   site: config.siteUrl,
-  integrations: [solid(), mdx(), UnoCSS({ injectReset: true })],
+  integrations: [mdx(), UnoCSS({ injectReset: true })],
 });
