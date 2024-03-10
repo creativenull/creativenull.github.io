281737e803df49f2d76b4e868838097e8674ebe1
Arnold Chand <creativenull@outlook.com>
2024-03-10 08:50:06 -0400

    fix: separate astro w/ tsserver

--- .vimrc.lua
+++ .vimrc.lua
@@ -1,15 +1,16 @@
 -- Powered by projectlocal.vim
 -- https://github.com/creativenull/projectlocal.vim
-local user_config = require("projectlocal.lsp").get_config()
+local pl = require("projectlocal.lsp")
 
-require("lspconfig").astro.setup(vim.tbl_extend("force", user_config, {
-  filetypes = { "astro", "typescript", "typescriptreact", "javascript", "javascriptreact", "markdown" },
+require("lspconfig").astro.setup(pl.get_config({
   init_options = {
     typescript = {
       tsdk = string.format("%s/node_modules/typescript/lib", vim.call("getcwd")),
     },
   },
 }))
+
+require('lspconfig').tsserver.setup(pl.get_config())
 
 -- Spell check for md files
 vim.api.nvim_create_autocmd("FileType", {
