dbeaeed86168d086cc0a2aa5eccb3124b6458011
Arnold Chand <creativenull@outlook.com>
2024-05-04 17:24:51 -0400

    fix: let web.nvim handle lsp setup

--- .vimrc.lua
+++ .vimrc.lua
@@ -2,16 +2,6 @@
 -- https://github.com/creativenull/projectlocal.vim
 local pl = require("projectlocal.lsp")
 
-require("lspconfig").astro.setup(pl.get_config({
-  init_options = {
-    typescript = {
-      tsdk = string.format("%s/node_modules/typescript/lib", vim.call("getcwd")),
-    },
-  },
-}))
-
-require('lspconfig').tsserver.setup(pl.get_config())
-
 -- Spell check for md files
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown" },
