27aec9a89bc68f80848ec68a8329017465bc7291
Arnold Chand <creativenull@outlook.com>
2024-06-23 12:26:51 -0400

    fix: use web.nvim

--- .vimrc.lua
+++
@@ -1,9 +0,0 @@
--- Powered by projectlocal.vim
--- https://github.com/creativenull/projectlocal.vim
-local pl = require("projectlocal.lsp")
-
--- Spell check for md files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown" },
-  command = "setlocal spell",
-})
